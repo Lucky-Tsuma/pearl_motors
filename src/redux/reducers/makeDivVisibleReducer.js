a098002753872bdf23b3a4c2b7821820b294c14c
Lucky-Tsuma <tsumahmumba@gmail.com>
2021-11-03 13:28:22 +0300

    Start working on cart

---
+++ src/redux/reducers/makeDivVisibleReducer.js
@@ -0,0 +1,16 @@
+// import { SHOW_DIV_QUANTITY } from "../types"
+// import ProductsSection from "../../components/ProductsSection";
+
+// const initialState = {
+//     display: "none"
+// }
+
+// const makeDivVisibleReducer = ( state = initialState, {type, payload} ) => {
+//     switch(type) {
+//         case SHOW_DIV_QUANTITY:
+//             const divToShow = ProductsSectio
+
+//     }
+// }
+
+// export default makeDivVisibleReducer;
---
+++ src/redux/types.js
@@ -0,0 +1,1 @@
+export const SHOW_DIV_QUANTITY = 'SHOW_DIV_QUANTITY';
