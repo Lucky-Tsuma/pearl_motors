2ef002b49d3793c040b7e50d10937b8e2559d9bf
Lucky-Tsuma <tsumahmumba@gmail.com>
2021-11-03 13:25:27 +0300

    Use array to store car details

--- src/components/ProductsSection.js
+++ src/components/ProductsSection.js
@@ -1,6 +1,10 @@
 import { Card } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 
+import React, { useState } from "react";
+import { useDispatch } from 'react-redux';
+import { makeDivVisible } from '../redux/actions/divVisibilityAction';
+
 import audi from '../assets/images/audiTT.jpg';
 import lambo2 from '../assets/images/lamboCOUNTACH2.jpg';
 import maybach from '../assets/images/maybach.jpg';
@@ -9,119 +13,82 @@ import lexus from '../assets/images/lexus.jpg';
 import chr2 from '../assets/images/toyotaC-HR2.jpg';
 
 const ProductsSection = () => {
+
+    const cars = [
+        {
+            name : "Toyota Prius",
+            mileage : "Mileage: 1000",
+            year : "Year of production: 2017",
+            price : "Price: $15650",
+            id : "toyota_prius",
+            image: prius2
+        },
+        {
+            name : "Lamborghini Countach",
+            mileage : "Mileage: 8000",
+            year : "Year of production: 2010",
+            price : "Price: $910000",
+            id : "lambo",
+            image: lambo2
+        },
+        {
+            name : "Audi TT",
+            mileage : "Mileage: 10000",
+            year : "Year of production: 2021",
+            price : "Price: $49800",
+            id : "audi",
+            image : audi
+        },
+        {
+            name : "Mercedes Maybach",
+            mileage : "Mileage: 13550",
+            year : "Year of production: 2016",
+            price : "Price: $170000",
+            id : "maybach",
+            image : maybach
+        },
+        {
+            name : "Lexus LX 570",
+            mileage : "Mileage: 12250",
+            year : "Year of production: 2018",
+            price : "Price: $115000",
+            id : "lexus",
+            image : lexus
+        },
+        {
+            name : "Toyota C-HR",
+            mileage : "Mileage: 11500",
+            year : "Year of production: 2017",
+            price : "Price: $23580",
+            id : "chr",
+            image : chr2
+        }
+    ];
+
+    // const dispatch = useDispatch();
     
     return (
         <div class = "products_section">
-        <Card>
-            <Card.Img variant="top" src={prius2} />
-            <Card.Body>
-            <Card.Title>Toyota Prius</Card.Title>
-            <Card.Text>
-                <p>Mileage: 10000</p>
-                <p>Year of production: 2017</p>
-                <p id="price">Price: $15650</p>
-            </Card.Text>
-            <Button variant="primary">Add to Cart</Button>
-            <div className="div_quantity">
-                <Button className="btn_plus">+</Button>
-                <input type="number" className="input_quantity" />
-                <Button className="btn_minus">-</Button>
-            </div>
-            </Card.Body>
-        </Card>
-
-        <Card>
-            <Card.Img variant="top" src={lambo2} />
-            <Card.Body>
-            <Card.Title>Lamborghini Countach</Card.Title>
-            <Card.Text>
-                <p>Mileage: 8000</p>
-                <p>Year of production: 2010</p>
-                <p id="price">Price: $910000</p>
-            </Card.Text>
-            <Button variant="primary">Add to Cart</Button>
-            <div className="div_quantity">
-                <Button className="btn_plus">+</Button>
-                <input type="number" className="input_quantity" />
-                <Button className="btn_minus">-</Button>
-            </div>
-            </Card.Body>
-        </Card>
-
-        <Card>
-            <Card.Img variant="top" src={audi} />
-            <Card.Body>
-            <Card.Title>Audi TT</Card.Title>
-            <Card.Text>
-                <p>Mileage: 10000</p>
-                <p>Year of production: 2021</p>
-                <p id="price">Price: $49800</p>
-            </Card.Text>
-            <Button variant="primary">Add to Cart</Button>
-            <div className="div_quantity">
-                <Button className="btn_plus">+</Button>
-                <input type="number" className="input_quantity" />
-                <Button className="btn_minus">-</Button>
-            </div>
-            </Card.Body>
-        </Card>
-
-        <Card>
-            <Card.Img variant="top" src={maybach} />
-            <Card.Body>
-            <Card.Title>Mercedes Maybach</Card.Title>
-            <Card.Text>
-                <p>Mileage: 13550</p>
-                <p>Year of production: 2016</p>
-                <p id="price">Price: $170000</p>
-            </Card.Text>
-            <Button variant="primary">Add to Cart</Button>
-            <div className="div_quantity">
-                <Button className="btn_plus">+</Button>
-                <input type="number" className="input_quantity" />
-                <Button className="btn_minus">-</Button>
-            </div>
-            </Card.Body>
-        </Card>
-
-        <Card>
-            <Card.Img variant="top" src={chr2} />
-            <Card.Body>
-            <Card.Title>Toyota C-HR</Card.Title>
-            <Card.Text>
-                <p>Mileage: 11500</p>
-                <p>Year of production: 2017</p>
-                <p id="price">Price: $23580</p>
-            </Card.Text>
-            <Button variant="primary">Add to Cart</Button>
-            <div className="div_quantity">
-                <Button className="btn_plus">+</Button>
-                <input type="number" className="input_quantity" />
-                <Button className="btn_minus">-</Button>
-            </div>
-            </Card.Body>
-        </Card>
-
-        <Card>
-            <Card.Img variant="top" src={lexus} />
-            <Card.Body>
-            <Card.Title>Lexus LX 570</Card.Title>
-            <Card.Text>
-                <p>Mileage: 12250</p>
-                <p>Year of production: 2018</p>
-                <p id="price">Price: $115000</p>
-            </Card.Text>
-            <Button variant="primary">Add to Cart</Button>
-            <div className="div_quantity">
-                <Button className="btn_plus">+</Button>
-                <input type="number" className="input_quantity" />
-                <Button className="btn_minus">-</Button>
-            </div>
-            </Card.Body>
-        </Card>
-        </div>
-
-        
+            {cars.map((car) => (
+                <Card>
+                <Card.Img variant="top" src={car.image} />
+                <Card.Body>
+                <Card.Title>{car.name}</Card.Title>
+                <Card.Text>
+                    <p>{car.mileage}</p>
+                    <p>{car.year}</p>
+                    <p id="price">{car.price}</p>
+                </Card.Text>
+                <Button variant="primary">Add to Cart</Button>
+                <div className="div_quantity" id={car.id}>
+                    <Button className="btn_plus">+</Button>
+                    <input type="text" className="input_quantity" />
+                    <Button className="btn_minus">-</Button>
+                </div>
+                </Card.Body>
+                </Card>
+            ))}
+                </div>
         );
     }
     
