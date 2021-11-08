d5a82a91019e9aa6fcb1010be7197f837a167625
Lucky-Tsuma <tsumahmumba@gmail.com>
2021-11-08 16:15:18 +0300

    Separate file for car details

---
+++ src/assets/data/carsData.js
@@ -0,0 +1,58 @@
+import audi from '../images/audiTT.jpg';
+import lambo2 from '../images/lamboCOUNTACH2.jpg';
+import maybach from '../images/maybach.jpg';
+import prius2 from '../images/toyotaPRIUS2.jpg';
+import lexus from '../images/lexus.jpg';
+import chr2 from '../images/toyotaC-HR2.jpg';
+
+
+export const cars = [
+    {
+        name : "Toyota Prius",
+        mileage : "Mileage: 1000",
+        year : "Year of production: 2017",
+        price : "Price: $15650",
+        id : "toyota_prius",
+        image: prius2
+    },
+    {
+        name : "Lamborghini Countach",
+        mileage : "Mileage: 8000",
+        year : "Year of production: 2010",
+        price : "Price: $910000",
+        id : "lambo",
+        image: lambo2
+    },
+    {
+        name : "Audi TT",
+        mileage : "Mileage: 10000",
+        year : "Year of production: 2021",
+        price : "Price: $49800",
+        id : "audi",
+        image : audi
+    },
+    {
+        name : "Mercedes Maybach",
+        mileage : "Mileage: 13550",
+        year : "Year of production: 2016",
+        price : "Price: $170000",
+        id : "maybach",
+        image : maybach
+    },
+    {
+        name : "Lexus LX 570",
+        mileage : "Mileage: 12250",
+        year : "Year of production: 2018",
+        price : "Price: $115000",
+        id : "lexus",
+        image : lexus
+    },
+    {
+        name : "Toyota C-HR",
+        mileage : "Mileage: 11500",
+        year : "Year of production: 2017",
+        price : "Price: $23580",
+        id : "chr",
+        image : chr2
+    }
+];
